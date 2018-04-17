f8935eb76b7ae2d6515469e08c50c4b6a94c98e9
Brent Connor <wmconnor1986@gmail.com>
2018-04-16 22:12:33 -0400

    Start building Connection Brake seeder

--- database/seeds/ConnectionBrakeSeeder.php
+++ database/seeds/ConnectionBrakeSeeder.php
@@ -11,6 +11,83 @@ class ConnectionBrakeSeeder extends Seeder
      */
     public function run()
     {
-        //
+      DB::table('connection_brake')->delete();
+
+      $brake_connections = array(
+        array(
+            'connection_id' => 1,
+            'brake_id' => 1,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'connection_id' => 1,
+            'brake_id' => 2,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'connection_id' => 1,
+            'brake_id' => 3,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'connection_id' => 1,
+            'brake_id' => 4,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'connection_id' => 2,
+            'brake_id' => 5,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'connection_id' => 3,
+            'brake_id' => 5,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'connection_id' => 4,
+            'brake_id' => 6,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+
+        array(
+            'connection_id' => 2,
+            'brake_id' => 6,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'connection_id' => 1,
+            'brake_id' => 7,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'connection_id' => 3,
+            'brake_id' => 7,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'connection_id' => 1,
+            'brake_id' => 8,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'connection_id' => 3,
+            'brake_id' => 8,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+      );
+      DB::table('connection_brake')->insert($brake_connections);
     }
 }
