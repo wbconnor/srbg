3dbbbdebafaacc00390a285a191d24ea63788880
Brent Connor <wmconnor1986@gmail.com>
2018-04-28 21:23:31 -0400

    build out m2m seeders for shifters

--- database/seeds/CompatibilityShifterSeeder.php
+++ database/seeds/CompatibilityShifterSeeder.php
@@ -11,6 +11,120 @@ class CompatibilityShifterSeeder extends Seeder
      */
     public function run()
     {
-        //
+      DB::table('compatibility_shifter')->delete();
+
+      $shifter_compatibilities = array(
+        array(
+            'compatibility_id' => 5,
+            'shifter_id' => 1,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'compatibility_id' => 1,
+            'shifter_id' => 1,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'compatibility_id' => 4,
+            'shifter_id' => 1,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'compatibility_id' => 5,
+            'shifter_id' => 2,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'compatibility_id' => 5,
+            'shifter_id' => 3,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'compatibility_id' => 5,
+            'shifter_id' => 4,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'compatibility_id' => 5,
+            'shifter_id' => 5,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'compatibility_id' => 5,
+            'shifter_id' => 6,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'compatibility_id' => 5,
+            'shifter_id' => 7,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'compatibility_id' => 5,
+            'shifter_id' => 8,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'compatibility_id' => 1,
+            'shifter_id' => 9,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'compatibility_id' => 3,
+            'shifter_id' => 9,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'compatibility_id' => 4,
+            'shifter_id' => 9,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'compatibility_id' => 5,
+            'shifter_id' => 9,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+
+        array(
+            'compatibility_id' => 5,
+            'shifter_id' => 10,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+
+        array(
+            'compatibility_id' => 1,
+            'shifter_id' => 11,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'compatibility_id' => 4,
+            'shifter_id' => 11,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+        array(
+            'compatibility_id' => 5,
+            'shifter_id' => 11,
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ),
+      );
+      DB::table('compatibility_shifter')->insert($shifter_compatibilities);
     }
 }
